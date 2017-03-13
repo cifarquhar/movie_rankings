db007a8fbda497040b0adf2d42f3fc7ea6959b53
Colin Farquhar <cifarquhar@gmail.com>
2017-03-13 18:10:53 +0000

    add findByRank method

--- Movies/app/src/main/java/com/codeclan/movies/Rankings.java
+++ Movies/app/src/main/java/com/codeclan/movies/Rankings.java
@@ -10,40 +10,18 @@ public class Rankings {
 
     ArrayList<Movie> movies;
 
-    public Rankings(){
-        movies = new ArrayList<Movie>();
-//        populateList();
+    public Rankings(ArrayList<Movie> movieList){
+        movies = new ArrayList<Movie>(movieList);
     }
 
-//    public void populateList(){
-//        Movie movie1 = new Movie("Star Wars", "Sci-Fi");
-//        Movie movie2 = new Movie("The Godfather","Gangster");
-//        Movie movie3 = new Movie("Saving Private Ryan","War");
-//        Movie movie4 = new Movie("Lord of the Rings","Fantasy");
-//        Movie movie5 = new Movie("28 Days Later","Horror");
-//        Movie movie6 = new Movie("Toy Story","Animated");
-//        Movie movie7 = new Movie("Pulp Fiction","Action");
-//        Movie movie8 = new Movie("Fight Club","Action");
-//        Movie movie9 = new Movie("Raiders of the Lost Ark","Adventure");
-//        Movie movie10 = new Movie("The Lego Movie","Animated");
-//
-//        Movie[] moviesToAdd = {
-//                movie1,
-//                movie2,
-//                movie3,
-//                movie4,
-//                movie5,
-//                movie6,
-//                movie7,
-//                movie8,
-//                movie9,
-//                movie10
-//        };
-//
-//        for(Movie movie : moviesToAdd) {
-//            movies.add(movie);
-//        }
-//    }
+    public Movie findByRank(int ranking){
+        for (int i = 0; i < movies.size(); i++) {
+            if (i == ranking) {
+                return movies.get(i);
+            }
+        }
+        return null;
+    }
 
 
 
--- Movies/app/src/test/java/com/codeclan/movies/RankingsTest.java
+++ Movies/app/src/test/java/com/codeclan/movies/RankingsTest.java
@@ -1,6 +1,10 @@
 package com.codeclan.movies;
 
 import org.junit.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,23 +14,55 @@ import static org.junit.Assert.*;
 public class RankingsTest {
 
     Rankings movieRankings;
+    ArrayList<Movie> movieList;
     Movie movie1;
+    Movie movie2;
+    Movie movie3;
+    Movie movie4;
+    Movie movie5;
+    Movie movie6;
+    Movie movie7;
+    Movie movie8;
+    Movie movie9;
+    Movie movie10;
 
 
     @Before
     public void Before(){
-        movieRankings = new Rankings();
+        movieList = new ArrayList<Movie>();
         movie1 = new Movie("Star Wars","Sci-Fi",1);
+        movie2 = new Movie("The Godfather","Gangster",2);
+        movie3 = new Movie("Saving Private Ryan","War",3);
+        movie4 = new Movie("28 Days Later","Horror",4);
+        movie5 = new Movie("Toy Story","Animated",5);
+        movie6 = new Movie("Raiders of the Lost Ark","Adventure",6);
+        movie7 = new Movie("The Lego Movie","Animated",7);
+        movie8 = new Movie("Batman","Action",8);
+        movie9 = new Movie("Fight Club","Action",9);
+        movie10 = new Movie("The Great Escape","War",10);
+        movieList.add(movie1);
+        movieList.add(movie2);
+        movieList.add(movie3);
+        movieList.add(movie4);
+        movieList.add(movie5);
+        movieList.add(movie6);
+        movieList.add(movie7);
+        movieList.add(movie8);
+        movieList.add(movie9);
+        movieList.add(movie10);
+        movieRankings = new Rankings(movieList);
     }
 
-//    @Test
-//    public void newListNotEmpty(){
-//        assertNotNull(movieRankings.movies.get(0));
-//    }
-//
-//    @Test
-//    public void canFindMovieRanking(){
-//
-//    }
+    @Test
+    public void newListNotEmpty(){
+        assertNotNull(movieRankings.movies.get(0));
+    }
+
+    @Test
+    public void canFindMovieByRanking(){
+        Movie testMovie = movieRankings.findByRank(1);
+        assertEquals("Star Wars",testMovie.getTitle());
+
+    }
 
 }
